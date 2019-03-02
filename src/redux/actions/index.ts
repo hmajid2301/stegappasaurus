f6a766782eab5c0a3d52093af5a24128e8ff1348
Haseeb Majid <hmajid2301@gmail.com>
2019-03-02 12:01:23 +0000

    :sparkles: Added Reset Button
    You can reset preferences back to default by pressing a button in the settings page. This button calls a function which sets the state back to it's initial state in the Redux store. This tutorial helped,
    https://alligator.io/redux/reset-state-redux/.

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,18 +1,19 @@
 import actionCreatorFactory from "typescript-fsa";
 
-import {
-  AlgorithmNames,
-  PrimaryColorNames
-} from "~/util/interfaces";
+import { AlgorithmNames, PrimaryColorNames } from "~/util/interfaces";
 
 import {
+  RESET_PREFERENCES,
   SELECT_ALGORITHM,
   TOGGLE_AUTOMATIC,
   TOGGLE_DARK_THEME,
   TOGGLE_PRIMARY_COLOR
 } from "./actionTypes";
+import { ReadStream } from "fs";
 
 const actionCreator = actionCreatorFactory();
+
+export const resetPreferences = actionCreator<{}>(RESET_PREFERENCES);
 
 export const selectAlgorithm = actionCreator<{ algorithm: AlgorithmNames }>(
   SELECT_ALGORITHM
