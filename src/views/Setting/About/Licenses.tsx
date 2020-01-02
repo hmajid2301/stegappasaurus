2de9ef4a01add950132a876df75a26def2bc7917
Haseeb Majid <hmajid2301@gmail.com>
2020-01-02 21:34:24 +0000

    :lipstick: Delete unncessary part in licenses
    In `licenses.ts` delete the redundant markdown.

--- src/views/Setting/About/Licenses.tsx
+++ src/views/Setting/About/Licenses.tsx
@@ -3,6 +3,7 @@ import {FlatList, Linking, TouchableOpacity} from 'react-native';
 import {ListItem} from 'react-native-elements';
 
 import license from '~/data/licenses.json';
+import {ThemeContext} from '~/providers/ThemeContext';
 
 interface License {
   licenses: string;
@@ -12,7 +13,14 @@ interface License {
   name: string;
 }
 
+interface LicenseItem {
+  item: License;
+}
+
 export default class Licenses extends React.Component<{}, {}> {
+  public static contextType = ThemeContext;
+  public context!: React.ContextType<typeof ThemeContext>;
+
   public render() {
     return (
       <FlatList
@@ -34,11 +42,14 @@ export default class Licenses extends React.Component<{}, {}> {
 
   private setKey = (item: License) => item.repository;
 
-  private renderItem = (item: any) => (
+  private renderItem = (item: LicenseItem) => (
     <TouchableOpacity onPress={this.openLink.bind(this, item.item.repository)}>
       <ListItem
         title={item.item.name}
         subtitle={item.item.licenses}
+        containerStyle={{backgroundColor: this.context.theme.background}}
+        titleStyle={{color: this.context.theme.color}}
+        subtitleStyle={{color: this.context.theme.color}}
         topDivider={true}
         bottomDivider={true}
         testID="license"
