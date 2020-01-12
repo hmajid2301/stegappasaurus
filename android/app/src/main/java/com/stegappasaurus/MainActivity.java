77c456b2a27b889d20d1cf820ae95a8d6494f7de
Haseeb Majid <hmajid2301@gmail.com>
2020-01-12 12:29:04 +0000

    :sparkles: :art: Added Dark Mode Splash Screen
    Added a dark mode version of the splash screen it
    checks if either dark mode is toggled on by the user,
    (stored in asyncstorage) or it is globally set in the
    UI.

--- android/app/src/main/java/com/stegappasaurus/MainActivity.java
+++ android/app/src/main/java/com/stegappasaurus/MainActivity.java
@@ -1,18 +1,37 @@
 package com.stegappasaurus;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.react.modules.storage.ReactDatabaseSupplier;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import org.devio.rn.splashscreen.SplashScreen;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
 
 public class MainActivity extends ReactActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String isDarkTheme = this.getAsyncStorageValue(getApplicationContext(), "@Theme");
+        Boolean isDark = Boolean.parseBoolean(isDarkTheme);
+        int globalDarkMode = (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK);
+
+        if(isDark == true || globalDarkMode == Configuration.UI_MODE_NIGHT_YES) {
+            setTheme(R.style.DarkTheme);
+        }
+        else {
+            setTheme(R.style.LightTheme);
+        }
+
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
@@ -42,5 +61,36 @@ public class MainActivity extends ReactActivity {
         Intent intent = new Intent("onConfigurationChanged");
         intent.putExtra("newConfig", newConfig);
         sendBroadcast(intent);
+    }
+
+    private String getAsyncStorageValue(Context context, String storageKey) {
+        SQLiteDatabase readableDatabase = null;
+        readableDatabase = ReactDatabaseSupplier.getInstance(context).getReadableDatabase();
+        Cursor catalystLocalStorage = readableDatabase.query("catalystLocalStorage", null, null, null, null, null, null);
+
+        try {
+            if (catalystLocalStorage.moveToFirst()) {
+
+                do {
+                    String key = catalystLocalStorage.getString(0);
+                    String value = catalystLocalStorage.getString(1);
+
+                    if(storageKey.equals(key)) {
+                        return value;
+                    }
+                } while (catalystLocalStorage.moveToNext());
+            }
+        } finally {
+            if (catalystLocalStorage != null) {
+                catalystLocalStorage.close();
+            }
+
+            if (readableDatabase != null) {
+                readableDatabase.close();
+            }
+        }
+
+        return "";
+
     }
 }
