a75e297f30f7f653ba7d5fcb55eacec368c7e444
Haseeb Majid <haseeb.majid@sky.uk>
2020-01-22 18:50:42 +0000

    work

--- util/publish-package.sh
+++ util/publish-package.sh
@@ -4,17 +4,13 @@ echo $CI_COMMIT_TAG
 
 if [[ $CI_COMMIT_TAG =~ release\/.* ]]; then
     echo "Release Tag: Promoting Package: Production" 
-    cd android && ./gradlew promoteArtifact \
-    --from-track beta --promote-track production \
-    --release-status completed
+    cd android && ./gradlew publish --track production
 elif [[ $CI_COMMIT_TAG =~ test\/.* ]]; then
     echo "Test Tag: Promoting Package: Beta" 
-    cd android && ./gradlew promoteArtifact \
-    --from-track internal --promote-track beta \
-    --release-status completed
+    cd android && ./gradlew publish --track beta
 elif [[ $CI_COMMIT_TAG =~ dev\/.* ]]; then
     echo "Dev Tag: Publishing Package: Alpha" 
-    cd android && ./gradlew publish
+    cd android && ./gradlew publish --track alpha
 else
     echo "No Tags: Publishing Package: Internal"
     cd android && ./gradlew publish --track internal
